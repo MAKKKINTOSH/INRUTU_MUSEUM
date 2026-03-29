3dcbf5bb7365595da2a22033264749d2ce562f1e
Van <us.ivan05@gmail.com>
2026-03-29 12:58:23 +0800

    фикс деплоя

--- server/museum/settings.py
+++ server/museum/settings.py
@@ -131,11 +131,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -196,8 +191,9 @@ SPECTACULAR_SETTINGS = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Static files settings (локальное хранение для разработки)
-STATIC_URL = '/static/'
+# Static files settings
+# /dj-static/ avoids conflict with React's /static/ (CRA build output)
+STATIC_URL = '/dj-static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
